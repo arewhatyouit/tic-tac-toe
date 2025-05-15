f17ccbf3493a3bf9614489a496eced65d203f5c9
arewhatyouit <isaac.noyes@gmail.com>
2025-05-14 23:24:03 -0700

    core logic for tic-tac-toe complete, need to add a win condition, and a reset

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
+//Step 1, draw a board.
 function Gameboard() {
-  //initial creation of a gameboard with 3 columns and 3 rows
+  //initial creation of a gameboard with 3 columns and 3 rows, board is held in array.
   const rows = 3;
   const columns = 3;
   const board = [];
 
-  //creating an instance of cellValue() to populate the board
-  const cellValue = CellValue();
-
+  //Use nested loops to initially populate the board with the value pulled from CellValue()
   for (let i = 0; i < rows; i++) {
     board[i] = [];
     for (let j = 0; j < columns; j++) {
@@ -14,18 +13,15 @@ function Gameboard() {
     }
   }
 
-  //method to acesss board to future UI
-  function outputBoard() {
-    return board;
-  }
 
-  //function for player to place token
   function placeToken(row, column, player) {
-    //Token placement validation
+    // First check if the cell is empty
     if (board[row][column].outputValue() !== 0) {
       console.log(`Invalid play, choose again.`);
+      return false;
     } else {
       board[row][column].assignCell(player);
+      return true;
     }
   }
 
@@ -34,7 +30,12 @@ function Gameboard() {
     const boardValues = board.map((row) =>
       row.map((cell) => cell.outputValue())
     );
-    console.log(boardValues);
+    return boardValues;
+  }
+
+  //Method to acesss board to future UI, this to be implemented later.
+  function outputBoard() {
+    return board;
   }
 
   return {
@@ -46,12 +47,15 @@ function Gameboard() {
 
 //function to set the cell value, 0 for initial value, 1 for player one, 2 for player two
 function CellValue() {
-  //set initial value of all cells to 0
+  //set initial value of all cells to 0 (0=blank)
   let value = 0;
+
   //create method to re-assignCell value to playernumber
   function assignCell(player) {
     value = player;
+    return value;
   }
+
   //create method to output final value to be used in Gameboard()
   function outputValue() {
     return value;
@@ -60,59 +64,78 @@ function CellValue() {
   return { assignCell, outputValue };
 }
 
-function gameFlow(playerOne = "Player One", playerTwo = "Player Two") {
-
+function GameFlow() {
+  //Creates the instance of the board we will be using for each playthrough
   const board = Gameboard();
 
   //Array to store player information, needs to include the players name and the players' tokens "x" and "o"
   const players = [
     {
-      name: playerOne,
+      name: "Player One",
       token: 1
     },
     {
-      name: playerTwo,
+      name: "Player Two",
       token: 2
     }
   ];
 
+  // console.log(board.printBoard());
+
   //Sets initial player activePlayer state to player 0
-  let activePlayer = players[0];
 
-  function changeActivePlayer() {
-    if (activePlayer === players[0]) {
-      activePlayer = players[1];
+  let currentPlayerIndex = 0;
+  let currentPlayer = players[0];
+
+  //Method to change the current player
+  function changeCurrentPlayer() {
+
+    if (currentPlayerIndex === 0) {
+      currentPlayerIndex = 1;
     } else {
-      activePlayer = players[0];
+      currentPlayerIndex = 0;
     }
-    console.log(`Active player is now ${players.name}.`);
+
+    currentPlayer = players[currentPlayerIndex];
+    return currentPlayer;
   }
 
-  function getActivePlayer() {
-    return activePlayer;
+  function getCurrentPlayer() {
+    return currentPlayer;
   }
+
+  function startNewRound() {
+    board.printBoard();
+    console.log(`${currentPlayer.name}'s turn.`);
+  }
+
+  function playRound(row, column) {
+    console.log(
+      `Placing ${currentPlayer.name}'s token on (${row}, ${column}).`
+    );
+    const moveSuccessful = board.placeToken(row, column, currentPlayer.token);
+    if (moveSuccessful) {
+      changeCurrentPlayer();
+    }
+    console.log(board.printBoard());
+    console.log(`${currentPlayer.name}'s turn.`);
+  }
+
+  //Start of new game message to console.
+  startNewRound();
+
+  return {
+    changeCurrentPlayer,
+    getCurrentPlayer,
+    playRound,
+    startNewRound
+  };
+}
+
+const game = GameFlow();
+function playGame(row, column) {
+  game.playRound(row, column);
 }
 
 // Logic for selecting a winnder
 // Logic to reset game once winner is selected
-
-// Test code
-const selectPlayerTest = gameFlow();
-console.log(selectPlayerTest.getActivePlayer())
-
-// const gameBoard = Gameboard();
-
-// console.log("Initial board:");
-// gameBoard.printBoard();
-
-// console.log("Player 2 placing token at [0,0]:");
-// gameBoard.placeToken(0, 0, 2);
-// gameBoard.printBoard();
-
-// console.log("Player 2 placing second token at [0,0]:");
-// gameBoard.placeToken(0, 0, 2);
-// gameBoard.printBoard();
-
-// console.log("Player 1 placing token at [0,1]:");
-// gameBoard.placeToken(0, 1, 1);
-// gameBoard.printBoard();
