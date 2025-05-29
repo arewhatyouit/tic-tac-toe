9956fa7b074457aab6291454425eeb1228dd6224
arewhatyouit <isaac.noyes@gmail.com>
2025-05-28 23:56:42 -0700

    Refactor game reset logic and fix scope issues

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-//Step 1, draw a board.
 function Gameboard() {
   //initial creation of a gameboard with 3 columns and 3 rows, board is held in array.
   const rows = 3;
   const columns = 3;
   const board = [];
 
-  //Use nested loops to initially populate the board with the value pulled from CellValue()
-  for (let i = 0; i < rows; i++) {
-    board[i] = [];
-    for (let j = 0; j < columns; j++) {
-      board[i].push(CellValue());
+  // Function to create 3x3 board using nested loops, one for row creation and one for column creation
+  function resetBoard() {
+    for (let i = 0; i < rows; i++) {
+      board[i] = [];
+      for (let j = 0; j < columns; j++) {
+        board[i].push(CellValue());
+      }
     }
   }
 
+  // Function to place a tolken in a specific space in the board using a 0,1,2 3x3 grid
   function placeToken(row, column, player) {
-    // First check if the cell is empty
     if (board[row][column].outputValue() !== 0) {
       console.log(`Invalid play, choose again.`);
       return false;
@@ -24,7 +25,7 @@ function Gameboard() {
     }
   }
 
-  // function to update gameboard with new values
+  // Function to print current board
   function printBoard() {
     const boardValues = board.map((row) =>
       row.map((cell) => cell.outputValue())
@@ -32,7 +33,7 @@ function Gameboard() {
     return boardValues;
   }
 
-  //Method to acesss board to future UI, this to be implemented later.
+  //Function to output board to use in furture UI
   function outputBoard() {
     return board;
   }
@@ -40,7 +41,8 @@ function Gameboard() {
   return {
     outputBoard,
     placeToken,
-    printBoard
+    printBoard,
+    resetBoard
   };
 }
 
@@ -67,9 +69,13 @@ function CellValue() {
   };
 }
 
+//Function to control the flow of the game.
 function GameFlow() {
   //Creates the instance of the board we will be using for each playthrough
   const board = Gameboard();
+
+  let currentPlayerIndex;
+  let currentPlayer;
 
   //Array to store player information, needs to include the players name and the players' tokens "x" and "o"
   const players = [
@@ -83,10 +89,23 @@ function GameFlow() {
     }
   ];
 
-  //Sets initial player activePlayer state to player 0
+  function resetAll() {
+    board.resetBoard();
+    resetPlayers();
+  }
 
-  let currentPlayerIndex = 0;
-  let currentPlayer = players[0];
+  resetAll();
+
+  function resetAll() {
+    board.resetBoard();
+    resetPlayers();
+  }
+
+  //Sets initial player activePlayer state to player 0
+  function resetPlayers() {
+    currentPlayerIndex = 0;
+    currentPlayer = players[0];
+  }
 
   //Method to change the current player
   function changeCurrentPlayer() {
@@ -110,6 +129,39 @@ function GameFlow() {
   }
 
   function playRound(row, column) {
+
+    let playAgain;
+    let checkToken = false;
+
+//TODO fix validation loop
+
+while (!checkToken) {
+  if (/* check if input is valid */) {
+    validInput = true;
+  } else {
+    playAgain = prompt('Invalid input, please type "y" or "n"');
+  }
+}
+
+    function checkPrompt() {
+    if (playAgain === "YES" || playAgain === "Y" || playAgain === "yes" || playAgain === "y") {
+      console.log(`debug: playAgain = ${playAgain}`)
+      resetAll();
+      console.log(`${currentPlayer.name}'s turn.`);
+    } else if (playAgain === "NO" || playAgain === "N" || playAgain === "no") {
+      playAgain = "n";
+      console.log(`debug: playAgain = ${playAgain}`)
+    } else if (playAgain !== "y" || playAgain !== "n") {
+      console.log(`debug: !== if triggered, playAgain = ${playAgain}`)
+      playAgain = prompt('Invalid input, please type "y" or "n"');
+    } else {
+      alert("Thanks for playing! See you again next time.");
+      return;
+    }
+    
+    return;
+  }
+
     console.log(
       `Placing ${currentPlayer.name}'s token on (${row}, ${column}).`
     );
@@ -118,45 +170,44 @@ function GameFlow() {
     const moveSuccessful = board.placeToken(row, column, currentPlayer.token);
     const winCheck = selectWinner();
 
-    const rowWin = winCheck.rowWin(row, currentPlayer.token);
-    const columnWin = winCheck.columnWin(column, currentPlayer.token);
+    const win = winCheck.win(row, column, currentPlayer.token);
 
-    console.log(`Current player is ${currentPlayer.token}.`)
-    console.log(`rowWin is ${rowWin}`);
-    console.log(`columnWin is ${columnWin}`);
+    console.log(`debug: Current player is ${currentPlayer.token}.`);
+    console.log(`debug: win is ${win}`);
 
-    if (rowWin === true || columnWin === true) {
-      console.log(`Player ${currentPlayer.token}, you've won. Play again?`);
+    if (win) {
+      console.log(`debug: Player ${currentPlayer.token} wins!`);
+      console.log(board.printBoard());
+      playAgain = prompt(`Yay! Player ${currentPlayer.token}, you've won. Play again? (y/n)`);
+      firstInput = false;
+      checkPrompt();
     }
 
     if (moveSuccessful) {
       changeCurrentPlayer();
     }
 
-    // Console log printing board post placement.
     console.log(board.printBoard());
 
     // Console log to confirm next player's turn
     console.log(`${currentPlayer.name}'s turn.`);
+
   }
+
+    // console.log("debug: invalidInput() triggered");
+    // let playAgain = prompt('Invalid input, please type "y" or "n"');
+
 
   function getBoard() {
     return board.printBoard();
   }
 
-  //TODO Create module in function "selectWinner" for diagonals.
+  //Function selectWinner contains the modules columnWin, rowWin and diagonalWin to handle each win condition.
   function selectWinner() {
-
+    //Logic for handling wins with rows
     function rowWin(row, player) {
       const winnerBoard = board.printBoard(row);
 
-      // const winnerBoard = {
-      //   0: [1, 1, 1],
-      //   1: [0, 0, 0],
-      //   2: [0, 0, 0]
-      // }
-
-      console.log(winnerBoard, player);
       let winTrue = true;
       for (element of winnerBoard[row]) {
         if (element !== player) {
@@ -167,18 +218,12 @@ function GameFlow() {
       return winTrue;
     }
 
+    //Logic for handling wins with columns
     function columnWin(column, player) {
-      // const winnerBoard = {
-      //   0: [1, 0, 0],
-      //   1: [1, 0, 0],
-      //   2: [1, 0, 0]
-      // }
-
       const winnerBoard = board.printBoard(column);
-      console.log(winnerBoard, player);
       let winTrue = true;
 
-      for (let i = 0; i < 2; i++) { //FIXME: Running into false win condition when there is a 1 in r1c1 and r2c1.
+      for (let i = 0; i < 3; i++) {
         if (winnerBoard[i][column] !== player) {
           winTrue = false;
           break;
@@ -187,10 +232,41 @@ function GameFlow() {
       return winTrue;
     }
 
-    return {
-      rowWin,
-      columnWin
-    };
+    //Logic for handling wins with diagonals
+    function diagonalWin(player) {
+      const winnerBoard = board.printBoard();
+      let winTrue = false;
+
+      if (
+        winnerBoard[0][2] === player &&
+        winnerBoard[1][1] === player &&
+        winnerBoard[2][0] === player
+      ) {
+        winTrue = true;
+      } else if (
+        winnerBoard[0][0] === player &&
+        winnerBoard[1][1] === player &&
+        winnerBoard[2][2] === player
+      ) {
+        winTrue = true;
+      }
+      return winTrue;
+    }
+
+    function win(row, column, player) {
+      const rowTrue = rowWin(row, player);
+      const columnTrue = columnWin(column, player);
+      const diagonalTrue = diagonalWin(player);
+      let win = false;
+
+      if (rowTrue || columnTrue || diagonalTrue) {
+        win = true;
+      }
+
+      return win;
+    }
+
+    return { win };
   }
 
   //Start of new game message to console.
@@ -202,12 +278,47 @@ function GameFlow() {
     playRound,
     startNewRound,
     getBoard,
-    selectWinner
+    selectWinner,
+    resetPlayers
   };
 }
 
+// function handleReset() {
+//   // Setup reset as a function wide variable
+//   let reset = "";
+
+//   function resetGame() {
+//     if (reset === "y") {
+//       startGame();
+//     } else if (reset === null || reset == "") {
+//       invalid();
+//     }
+//   }
+
+//   function invalid() {
+//     reset = prompt(
+//       `Invalid input, please type either "y" or "n". Play again? (y/n)`
+//     );
+//     if (reset === "y") {
+//       startGame();
+//     } else if (reset === null || reset == "") {
+//       invalid();
+//     }
+//   }
+
+//   function startGame() {
+//     game = GameFlow();
+//   }
+
+//   return {
+//     resetGame,
+//     startGame,
+//     invalid
+//   };
+// }
+
 // the variable "game" and function "playGame" are to help debug the game in the console, and won't be used later on
-const game = GameFlow();
+let game = GameFlow();
 function playGame(row, column) {
   game.playRound(row, column);
 }
